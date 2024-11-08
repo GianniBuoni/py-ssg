df54915ff04d9447db565241e4eb2ce514989c0a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-08 11:55:27 -0500

    Fixed text_node_to_html_node function

--- src/lib/textnode.py
+++ src/lib/textnode.py
@@ -27,18 +27,18 @@ class TextNode:
 
 
 def text_node_to_html_node(text_node):
-    match (text_node.text):
-        case (TextType.TEXT.value):
+    match (text_node.text_type):
+        case (TextType.TEXT):
             return LeafNode(None, text_node.text)
-        case (TextType.BOLD.value):
+        case (TextType.BOLD):
             return LeafNode("b", text_node.text)
-        case (TextType.ITALIC.value):
+        case (TextType.ITALIC):
             return LeafNode("i", text_node.text)
-        case (TextType.CODE.value):
+        case (TextType.CODE):
             return LeafNode("code", text_node.text)
-        case (TextType.LINK.value):
+        case (TextType.LINK):
             return LeafNode("a", text_node.text, {"href": text_node.url})
-        case (TextType.IMAGE.value):
+        case (TextType.IMAGE):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Invalid text type: {text_node.text_type}")
