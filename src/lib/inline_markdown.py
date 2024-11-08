579c49c9114cfc8b17bbcf0d7cc4634a8eee6f58
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-11-08 13:37:35 -0500

    Added regex helper function

--- src/lib/inline_markdown.py
+++ src/lib/inline_markdown.py
@@ -1,4 +1,5 @@
 from lib.textnode import TextType, TextNode
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list:
     new_nodes = []
@@ -25,3 +26,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list:
         
         new_nodes.extend(split_nodes)
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
